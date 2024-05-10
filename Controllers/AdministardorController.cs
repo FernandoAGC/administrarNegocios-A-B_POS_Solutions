bc50e1ad202a4730dfd2e4d29843c7f30264b303
FernandoAGC <fernando.a.galicia.c@gmail.com>
2024-05-10 11:23:44 -0600

    add: listado de 20 negocios en vista Administrador
    La vista principal de Administrador ahora mustra los ultimos 20 negocios agregados, mostrando la informacion de cada uno. Ademas se agrego la funcionalidad para filtrar por nombre de negocio.

--- Controllers/AdministardorController.cs
+++ Controllers/AdministardorController.cs
@@ -1,5 +1,6 @@
 using administrarNegocios_A_B_POS_Solutions.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace administrarNegocios_A_B_POS_Solutions.Controllers;
 
@@ -13,8 +14,19 @@ public class AdministradorController : Controller
     }
 
     [HttpGet]
-    public IActionResult Inicio()
+    public async Task<IActionResult> Inicio(string filtro)
     {
-        return View();
+        // Obtener los ultimos 20 negocios
+        var negocios = _context.Negocios.Include(n => n.Usuario) // Incluye los datos del usuario asociado al negocio
+            .OrderByDescending(n => n.Id) // Ordena por ID de manera descendente
+            .Take(20); // Limita a 20 resultados
+
+        // Filtrar por nombre de negocio si se proporciona una cadena de filtro
+        if (!string.IsNullOrEmpty(filtro))
+        {
+            negocios = negocios.Where(n => n.Nombre.Contains(filtro));
+        }
+        // demanera asíncrona convertir los resultados en una lista
+        return View(await negocios.ToListAsync());
     }
 }
